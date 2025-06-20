adc0de37184173b559662cfc5f65c074e2a1515e
Ruslan Serebriakov <rsrbk1@gmail.com>
2025-06-20 23:15:25 +0100

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,9 @@ export const auth0Config = {
   domain: "dev-w0vuxd3zxhj28flo.uk.auth0.com",
   clientId: "OZKVYUdvGktgy5KAthZS8oBN97yC6zYW",
   authorizationParams: {
-    redirect_uri: typeof window !== 'undefined' ? window.location.origin : '',
+    redirect_uri: "https://frontend-k6tf.onrender.com",
     audience: "https://vibe-api"
   },
+  cacheLocation: "localstorage",
+  useRefreshTokens: true
 }; 
