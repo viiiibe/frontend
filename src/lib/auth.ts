912dbe227e4639cd524054e9edad32d7972fb7f6
Pavel Safronov <pv.safronov@gmail.com>
2025-06-21 14:31:05 +0000

    tmp audience remove

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,8 @@ export const auth0Config = {
   clientId: "8q5IimCLt3gDPVfoiF8fPGKr1tcXh9H3",
   authorizationParams: {
     redirect_uri: "https://frontend-k6tf.onrender.com",
-    audience: "https://backend-bdv7.onrender.com"
+    // audience: "https://backend-bdv7.onrender.com",
+    // scope: "openid profile email"
   },
   cacheLocation: "localstorage" as const,
   useRefreshTokens: true,
